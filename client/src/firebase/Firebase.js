c163bf9d1e9b607da0d57ba6bc28b9c3a5ab6a40
jiaqili1997 <jiaqili1997@gmail.com>
2020-12-08 22:29:10 -0500

    firebase

---
+++ client/src/firebase/Firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyDmB-opvvCqxldTfywUGuvW8Gk5TBaB1aI",
+  authDomain: "fcs554finalproject.firebaseapp.com",
+  projectId: "fcs554finalproject",
+  storageBucket: "fcs554finalproject.appspot.com",
+  messagingSenderId: "808906833863",
+  appId: "1:808906833863:web:61f21310b711eaf51463e2"
+
+});
+
+export default firebaseApp;
