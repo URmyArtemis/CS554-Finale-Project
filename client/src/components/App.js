d1311521cfa6ee9cdb71fd4ea1ece933b432a2b7
Chaois <chuqiushi@outlook.com>
2020-12-16 16:43:18 -0500

    applo client

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,30 +1,39 @@
 import React from 'react';
 import '../App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { AuthProvider } from '../firebase/Auth';
+import PrivateRoute from './PrivateRoute';
 import Account from './Account';
 import Home from './Home';
 import Landing from './Landing';
 import Navigation from './Navigation';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
-import { AuthProvider } from '../firebase/Auth';
-import PrivateRoute from './PrivateRoute';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({ uri: 'http://localhost:4000' })
+});
+
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <Navigation />
-          </header>
-        </div>
-        <Route exact path="/" component={Landing} />
-        <PrivateRoute path="/home" component={Home} />
-        <PrivateRoute path="/account" component={Account} />
-        <Route path="/signin" component={SignIn} />
-        <Route path="/signup" component={SignUp} />
-      </Router>
-    </AuthProvider>
+    <ApolloProvider client={client}>
+      <AuthProvider>
+        <Router>
+          <div className="App">
+            <header className="App-header">
+              <Navigation />
+            </header>
+          </div>
+          <Route exact path="/" component={Landing} />
+          <PrivateRoute path="/home" component={Home} />
+          <PrivateRoute path="/account" component={Account} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+        </Router>
+      </AuthProvider>
+    </ApolloProvider>
   );
 }
 
