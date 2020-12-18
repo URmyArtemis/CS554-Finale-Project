86bc982637cc2f1b8434c3d2d6ab5c9fe0fb81f8
jiaqili1997 <jiaqili1997@gmail.com>
2020-12-17 22:03:08 -0500

    account

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import SignOutButton from './SignOut';
 import '../App.css';
 import ChangePassword from './ChangePassword';
+import { AuthContext } from '../firebase/Auth';
 
 function Account() {
+  const { currentUser } = useContext(AuthContext);
   return (
     <div>
       <h2>Account Page</h2>
+      <h2>{currentUser.displayName}</h2>
       <ChangePassword />
       <SignOutButton />
     </div>
